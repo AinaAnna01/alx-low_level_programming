3679eaa127e3385b87598918c7a29e08dcb9fe36
Your Name <you@example.com>
2023-02-24 14:06:04 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*print_square - Prints a triangle of squares according to parameter
+*print_triangle - Prints a triangle of squares according to parameter
 *@size: The size of the square triangle
 *Return: empty
 */
@@ -9,25 +9,25 @@
 void print_triangle(int size)
 
 {
-int x, y, z;
+	int x, y, z;
 
-if (size <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (x = 0; x < size; x++)
-{
-for (y = size - x; y > 1; y--)
-{
-_putchar(32);
-}
-for (z = 0; z <= x; z++)
-{
-_putchar(35);
-}
-_putchar('\n');
-}
-}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
+			}
+			for (z = 0; z <= x; z++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
 }
