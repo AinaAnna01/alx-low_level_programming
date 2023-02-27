8699c136d4324b6b7b938fc55b84182b7634ec24
Your Name <you@example.com>
2023-02-27 11:18:43 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /**
- * print_rev - Prints a string in reverse
- * @S: The string to print
- * Return: void
+ *print_rev - Prints a string in reverse
+ *@s: The string to print
+ *Return: void
 */
 
 void print_rev(char *s)
-
 {
 	int c = 0;
 	
 	while (s[c] != '\0')
 	{
-		c++;
+	c++;
 	}
 	for (c -= 1; c >= 0; c--)
 	{
-		putchar(s[c]);
+	putchar(s[c]);
 	}
 	putchar('\n');
 }
