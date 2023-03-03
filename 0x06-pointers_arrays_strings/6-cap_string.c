defd8df122a9df07f86dce6a9e6747241711a5f9
Your Name <you@example.com>
2023-03-02 19:30:46 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,34 +2,34 @@
 
 /**
  * cap_string - This capitalizes all the words in a string
- * @n: This is the string to be capitalized
+ * @str: This is the string to be capitalized
  * Return: The pointer to the changed string
  */
 
-char *cap_string(char *n)
+char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (n[index])
+	while (str[index])
 	{
-		while (!(n[index] >= 'a' && n[index] <= 'z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 		index++;
-		if (n[index - 1] == ' ' ||
-		n[index - 1] == '\t' ||
-		n[index - 1] == '\n' ||
-		n[index - 1] == ',' ||
-		n[index - 1] == ';' ||
-		n[index - 1] == '.' ||
-		n[index - 1] == '!' ||
-		n[index - 1] == '?' ||
-		n[index - 1] == '"' ||
-		n[index - 1] == '(' ||
-		n[index - 1] == ')' ||
-		n[index - 1] == '{' ||
-		n[index - 1] == '}' ||
+		if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
 		index == 0)
-		n[index] -= 32;
+		str[index] -= 32;
 		index++;
 	}
-return (n);
+return (str);
 }
